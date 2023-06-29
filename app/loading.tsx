20370b938b8c990dd6ce09466b94d14e588a2e68
Jorrit van der Heide <35707261+jorritvanderheide@users.noreply.github.com>
2023-06-29 16:44:32 +0200

    Add loading page home

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,17 +1,37 @@
 const Loading = () => {
   return (
-    <section className="mx-auto -mt-header-mobile h-[100svh] w-screen animate-pulse px-2 md:-mt-header md:px-4">
-      <article className="h-full pb-2 pt-9 md:pb-4 md:pt-7">
-        <div className="flex h-full flex-col gap-3">
-          <figure className="relative h-full w-full overflow-hidden rounded-3xl bg-gray-200 dark:bg-gray-800"></figure>
-          <div className="flex select-none justify-between gap-4 line-through decoration-gray-300 decoration-[1.25em] dark:decoration-gray-700 md:gap-0">
-            <p className="font-sora font-semibold uppercase">
-              final master project 2 - envisioning eindhoven
-            </p>
-            <p className="font-sora font-medium uppercase">1/5</p>
+    <section className="mb-4 mt-2 h-[calc(100svh_-_14em)] animate-pulse px-0 md:h-[calc(100vh_-_11em)]">
+      <div className="mx-auto flex h-full w-full flex-col gap-2 px-2 md:flex-row md:px-4">
+        <article className=" h-full w-full basis-1/5">
+          <div className="flex h-full w-full flex-col gap-3">
+            <figure className="relative h-full w-full overflow-hidden rounded-3xl bg-gray-200 dark:bg-gray-800"></figure>
           </div>
-        </div>
-      </article>
+        </article>
+
+        <article className=" h-full w-full basis-1/5">
+          <div className="flex h-full w-full flex-col gap-3">
+            <figure className="relative h-full w-full overflow-hidden rounded-3xl bg-gray-200 dark:bg-gray-800"></figure>
+          </div>
+        </article>
+
+        <article className=" h-full w-full basis-1/5">
+          <div className="flex h-full w-full flex-col gap-3">
+            <figure className="relative h-full w-full overflow-hidden rounded-3xl bg-gray-200 dark:bg-gray-800"></figure>
+          </div>
+        </article>
+
+        <article className=" h-full w-full basis-1/5">
+          <div className="flex h-full w-full flex-col gap-3">
+            <figure className="relative h-full w-full overflow-hidden rounded-3xl bg-gray-200 dark:bg-gray-800"></figure>
+          </div>
+        </article>
+
+        <article className=" h-full w-full basis-1/5">
+          <div className="flex h-full w-full flex-col gap-3">
+            <figure className="relative h-full w-full overflow-hidden rounded-3xl bg-gray-200 dark:bg-gray-800"></figure>
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
